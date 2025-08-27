6683a5d41e5b0d80136fb7057af6cd1fdd846da1
alihaider1145 <alihaider15112006@gmail.com>
2025-08-27 11:35:20 +0500

    Add the operate function

--- javascript.js
+++ javascript.js
@@ -1,3 +1,16 @@
+function operate(operand1,operator,operand2){
+    switch(operator){
+        case '+':
+            add(operand1,operand2);
+        case '-':
+            subtract(operand1,operand2);
+        case '*':
+            multiply(operand1,operand1);
+        case '/':
+            divide(operand1,operand2)
+    }
+}
+
 function add(a,b){
     return a+b;
 }
